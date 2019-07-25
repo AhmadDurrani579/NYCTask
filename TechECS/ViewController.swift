b01505726e6c2c1157beca0d6fdfd6b7fa8e8543
Ahmad <ahmaddurranitrg@gmail.com>
2019-07-25 17:27:15 +0400

    Initial Commit

---
+++ TechECS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechECS
+//
+//  Created by Ahmed Durrani on 25/07/2019.
+//  Copyright © 2019 TeachEase solution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
